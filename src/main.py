1ba1c09a254ecb9677d327fd91854daabed2d0ac
Ramazan Cetin <ramazancetinn@yandex.com>
2019-11-30 17:42:34 +0300

    moment js added db commit added

--- src/main.py
+++ src/main.py
@@ -9,11 +9,36 @@ mysql = MySQL(app)
 
 @app.route('/')
 def home():
-   cur = mysql.connection.cursor()
-   cur.execute("SELECT * FROM user;")
-   users = cur.fetchall()
+   # cur = mysql.connection.cursor()
+   # cur.execute("SELECT * FROM user;")
+   # users = cur.fetchall()
+   # cur.close()
    return render_template("home.html", **locals())
+@app.route('/add_income', methods=['POST'])
+def add_income():
+   income_date = request.json["income_date"]
+   income_type = request.json["income_type"]
+   income_explanation = request.json["income_explanation"]
+   income_price = request.json["income_price"]
+   cur = mysql.connection.cursor()
+   cur.execute(f"""INSERT INTO income (income_type, income_explanation, income_price, income_date)
+                                 VALUES ('{income_type}', '{income_explanation}', '{income_price}', '{income_date}')""")
+   mysql.connection.commit()
+   cur.close()
+   return "success"
 
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+   expense_date = request.json["expense_date"]
+   expense_type = request.json["expense_type"]
+   expense_explanation = request.json["expense_explanation"]
+   expense_price = request.json["expense_price"]
+   cur = mysql.connection.cursor()
+   cur.execute(f"""INSERT INTO expense (expense_type, expense_explanation, expense_price, expense_date)
+                                 VALUES ('{expense_type}', '{expense_explanation}', '{expense_price}', '{expense_date}')""")
+   mysql.connection.commit()
+   cur.close()
+   return "success"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ src/static/js/home.js
@@ -0,0 +1,64 @@
+
+$("#incomeSubmit").on("click", ()=>{
+    var income_date = moment().format("YYYY-MM-DD");
+    var income_type = $( "#incomeSelect option:selected" ).text();
+    var income_explanation = $("#incomeExplanation").val();
+    var income_price = $("#incomePrice").val();
+    income_data = {
+        income_date: income_date,
+        income_type: income_type,
+        income_explanation: income_explanation,
+        income_price: income_price
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/add_income",
+        contentType: 'application/json;charset=UTF-8',
+        data : JSON.stringify(income_data),
+        success: (res)=>{
+            console.log(res)
+        }
+    })
+})
+
+function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
+
+$("#expenseSubmit").on("click", ()=>{
+    var expense_date = moment().format("YYYY-MM-DD");
+    var expense_type = $( "#expenseSelect option:selected" ).text();
+    var expense_explanation = $("#expenseExplanation").val();
+    var expense_price = $("#expensePrice").val();
+    expense_data = {
+        expense_date: expense_date,
+        expense_type: expense_type,
+        expense_explanation: expense_explanation,
+        expense_price: expense_price
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/add_expense",
+        contentType: 'application/json;charset=UTF-8',
+        data : JSON.stringify(expense_data),
+        success: (res)=>{
+            console.log(res)
+        }
+})
+})
