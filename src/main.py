9dcffe6fc75af1504dd7660bf4f457831b88db8b
Ramazan Cetin <ramazancetinn@yandex.com>
2019-12-01 19:13:48 +0300

    user feedbacks added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 from flask_mysqldb import MySQL
 from datetime import datetime, timedelta, time
 from flask import jsonify
--- src/static/js/home.js
+++ src/static/js/home.js
@@ -4,6 +4,21 @@ $("#incomeSubmit").on("click", ()=>{
     var income_type = $( "#incomeSelect option:selected" ).text();
     var income_explanation = $("#incomeExplanation").val();
     var income_price = $("#incomePrice").val();
+
+    if (!income_price || income_price == null || income_price == undefined){
+        $("#incomePrice").addClass("notValid")
+    }
+    if(!income_explanation ){
+        $("#incomeExplanation").addClass("notValid")
+    }
+    if(income_type=="Gelir Seç"){
+        $("#incomeSelect").addClass("notValid")
+    }
+
+    if(!income_price || !income_type || !income_explanation ){
+        return false;
+    }
+
     income_data = {
         income_date: income_date,
         income_type: income_type,
@@ -17,7 +32,49 @@ $("#incomeSubmit").on("click", ()=>{
         contentType: 'application/json;charset=UTF-8',
         data : JSON.stringify(income_data),
         success: (res)=>{
-            console.log(res)
+            $(".alert").removeClass("none")
+            $("#alertText").text("Gelir Eklendi.")
+            $("#incomeCancel").click()
+        }
+    })
+})
+
+$("#expenseSubmit").on("click", ()=>{
+    var expense_date = moment().format("YYYY-MM-DD");
+    var expense_type = $( "#expenseSelect option:selected" ).text();
+    var expense_explanation = $("#expenseExplanation").val();
+    var expense_price = $("#expensePrice").val();
+    
+    if (!expense_price || expense_price == null || expense_price == undefined){
+        $("#expensePrice").addClass("notValid")
+    }
+    if(!expense_explanation ){
+        $("#expenseExplanation").addClass("notValid")
+    }
+    if(expense_type=="Gider Seç"){
+        $("#expenseSelect").addClass("notValid")
+    }
+
+    if(!expense_price || !expense_type || !expense_explanation ){
+        return false;
+    }
+    
+    expense_data = {
+        expense_date: expense_date,
+        expense_type: expense_type,
+        expense_explanation: expense_explanation,
+        expense_price: expense_price
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/add_expense",
+        contentType: 'application/json;charset=UTF-8',
+        data : JSON.stringify(expense_data),
+        success: (res)=>{
+            $(".alert").removeClass("none")
+            $("#alertText").text("Gider Eklendi.")
+            $("#expenseCancel").click()
         }
     })
 })
@@ -40,25 +97,22 @@ function validate(evt) {
     }
   }
 
-$("#expenseSubmit").on("click", ()=>{
-    var expense_date = moment().format("YYYY-MM-DD");
-    var expense_type = $( "#expenseSelect option:selected" ).text();
-    var expense_explanation = $("#expenseExplanation").val();
-    var expense_price = $("#expensePrice").val();
-    expense_data = {
-        expense_date: expense_date,
-        expense_type: expense_type,
-        expense_explanation: expense_explanation,
-        expense_price: expense_price
-    }
-    console.log(expense_data)
-    $.ajax({
-        type: "POST",
-        url: "/add_expense",
-        contentType: 'application/json;charset=UTF-8',
-        data : JSON.stringify(expense_data),
-        success: (res)=>{
-            console.log(res)
-        }
+    $("#incomeSelect").on("change", ()=>{
+        $("#incomeSelect").removeClass("notValid")
     })
-})
+    $("#incomeExplanation").on("change", ()=>{
+        $("#incomeExplanation").removeClass("notValid")
+    })
+    $("#incomePrice").on("change", ()=>{
+        $("#incomePrice").removeClass("notValid")
+    })
+
+    $("#expenseSelect").on("change", ()=>{
+        $("#expenseSelect").removeClass("notValid")
+    })
+    $("#expenseExplanation").on("change", ()=>{
+        $("#expenseExplanation").removeClass("notValid")
+    })
+    $("#expensePrice").on("change", ()=>{
+        $("#expensePrice").removeClass("notValid")
+    })
